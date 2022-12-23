b89cf02efdb6de9365973f4ec34a6791a4961107
tluciad <tluciad@gmail.com>
2022-12-23 00:05:36 -0500

    0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,8 +3,8 @@
 import sys
 Status = {'200': 0, '301': 0, '400': 0, '401': 0,
           '403': 0, '404': 0, '405': 0, '500': 0}
-count= 0
-size= 0
+count = 0
+size = 0
 
 for input in sys.stdin:
     ListInput = input.split(' ')
@@ -18,11 +18,11 @@ for input in sys.stdin:
     if count == 10:
         print("File size: {:d}".format(size))
         for key, value in sorted(Status.items()):
-            if  value != 0:
+            if value != 0:
                 print("{}: {:d}".format(key, value))
         count = 0
-   
+
 print("File size: {:d}".format(size))
 for key, value in sorted(Status.items()):
-    if  value != 0:
-        print("{}: {:d}".format(key, value))   
+    if value != 0:
+        print("{}: {:d}".format(key, value))
