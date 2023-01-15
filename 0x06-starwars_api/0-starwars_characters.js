31d2abff64f02cefb61f9e0cfda33d0c02dab546
tluciad <tluciad@gmail.com>
2023-01-15 18:52:15 -0500

    0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,17 +3,17 @@ const request = require('request');
 const MovieID = process.argv[2];
 const url = `https://swapi-api.hbtn.io/api/films/${MovieID}`;
 request(url, async (error, response, body) => {
-    if (!error) {
-        const characters = JSON.parse(body).characters;
-        for (const charId in characters) {
-            await new Promise((resolve, reject) => {
-                request(characters[charId], (error, response, body) => {
-                    if (!error) {
-                        console.log(JSON.parse(body).name);
-                        resolve();
-                    }
-                });
-            });
-        }
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    for (const charId in characters) {
+      await new Promise((resolve, reject) => {
+        request(characters[charId], (error, response, body) => {
+          if (!error) {
+            console.log(JSON.parse(body).name);
+            resolve();
+          }
+        });
+      });
     }
+  }
 });
